1063f8b14f00b9451759eb5eb4cc5ab9e2e43efd
StPaulsMultimedia <multimedia@stpauls.in>
2026-01-09 13:45:35 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,37 @@
-const cacheName = 'studio-v2';
-const filesToCache = ['./', 'index.html', 'manifest.json'];
+const CACHE_NAME = 'gods-word-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+// Install Service Worker and cache files
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+  self.skipWaiting(); // Forces the new service worker to take over immediately
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching new assets');
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Activate and remove old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
 
+// Fetch assets from cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
